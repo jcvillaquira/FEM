f9d23c4879c7f2e27f1e21e24fe3d4fccce625e5
CarlosContrerasQ12 <cadacoqu2@live.com>
2023-06-01 16:23:35 -0500

    SS

--- finite_elements.py
+++ finite_elements.py
@@ -9,3 +9,7 @@ print(node_coordinates[0])
 def get_basis_coefficients(n_coor):
     E=np.array([n_coor[:,0]**2,n_coor[:,0]*n_coor[:,1],n_coor[:,1]**2,n_coor[:,0],n_coor[:,1],np.ones(n_coor.shape[0])]).T
     return np.linalg.inv(E)
+
+def get_elemental_matrix(basis_coeff):
+    A=np.zeros((6,6))
+
