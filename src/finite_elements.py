6fba32bfc9dfb7bc6add4d50069a09012e8f032a
jcvillaquira <jc.villaquira@uniandes.edu.co>
2023-06-01 17:10:09 -0500

    rename notebook

--- src/finite_elements.py
+++ src/finite_elements.py
@@ -3,9 +3,6 @@ import numpy as np
 import quadrature
 from mesh import read_msh
 
-node_coordinates,connection_table=read_msh('L2.msh')
-print(node_coordinates[0])
-
 def get_basis_coefficients(n_coor):
     E=np.array([n_coor[:,0]**2,n_coor[:,0]*n_coor[:,1],n_coor[:,1]**2,n_coor[:,0],n_coor[:,1],np.ones(n_coor.shape[0])]).T
     return np.linalg.inv(E)
